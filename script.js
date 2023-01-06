f32abc6ec3499539904ed405577ef3db2aea61d0
Goldroger1514 <96831410+Goldroger1514@users.noreply.github.com>
2023-01-06 12:14:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
+const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random';
 let text = document.querySelector('.text');
 let input = document.querySelector('#input');
 let time = document.querySelector('.time');
